edb49d2b85e797a9764410bf3fe5aec89ee27534
Jeffrey Meyer <jam93t@mst.edu>
2025-03-24 01:45:12 -0500

    ignore drop tightening warning as it has incorrect --fix results

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![deny(clippy::pedantic, clippy::nursery)]
-#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::module_name_repetitions, clippy::significant_drop_tightening)]
 
 use std::fs::File;
 use std::io::Read;
